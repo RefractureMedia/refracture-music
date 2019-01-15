c3625f1fcff1be4d1402013bace580d7171952e8
Mulvey <dlmf1ccd@thompsonschools.org>
2019-01-15 16:18:40 -0700

    Testing

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,18 @@ function addStyleString(str) {
     node.innerHTML = str;
     document.body.appendChild(node);
 }
-
+// test
 document.onkeydown = function (e) {
     if (e.ctrlKey && e.which == 82) {
         location.reload();
     } else if(e.ctrlKey && e.shiftKey && e.which == 73) {
-        window.parent.inspect();
+        window.parent.inspect(); // ignore error, initiates inspect on window correctly
     }
 };
 
 function sidebar_toggle() {
     if (sidebar_open) {
-        addStyleString('.sidebar_content { display: none; transition: display 0.2s;} .sidebar { width: 0px !important; transition: width 0.2s;} .sidebar-top { width: 0px !important; transition: width 0.2s;} .sidebar-bottom { width: 0px !important; transition: width 0.2s;} .sidebar_toggle { margin-left: .5rem !important; transition: margin-left 0.2s;} div.sidebar_toggle_x { margin-top: -10rem !important; ; transition: margin-top 0.2s;} .bar { width: 78vw !important; transition: width 0.2s;}')
+        addStyleString('.sidebar_content { display: none; transition: display 0.2s;} .sidebar { width: 0px !important; transition: width 0.2s;} .sidebar-top { width: 0px !important; transition: width 0.2s;} .sidebar-bottom { width: 0px !important; transition: width 0.2s;} .sidebar_toggle { margin-left: .5rem !important; transition: margin-left 0.2s;} div.sidebar_toggle_x { margin-top: -10rem !important; transition: margin-top 0.2s;} .bar { width: 78vw !important; transition: width 0.2s;}')
         sidebar_open = false;
     } else {
         addStyleString('.sidebar_content { display: block; transition: display 0.2s;} .sidebar { width: 20vw !important; transition: width 0.2s;} .sidebar-top { width: 16.65vw !important; transition: width 0.2s;} .sidebar-bottom { width: 20vw !important; transition: width 0.2s;} .sidebar_toggle { margin-left: -3rem !important; transition: margin-left 0.2s;} div.sidebar_toggle_x { margin-top: .85rem !important; transition: margin-top 0.2s;} .bar { width: 61vw !important; transition: width 0.2s;}')
