eacabd5ab6e837d3032578bc0c4488321b1d92c8
Mulvey <dlmf1ccd@thompsonschools.org>
2019-01-24 09:25:56 -0700

    Work on Songs display

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -234,9 +234,7 @@ export default {
       },
       player: new Audio(""),
       webviewURL: "https://www.youtube.com/watch?v=LDU_Txk06tM",
-      //blob = fetch(URL.createObjectURL('./inject.js')).then(r => r.blob()),
       preload: `file:\\${require("path").resolve(__dirname, "./inject.js")}`
-      //webviewInject: URL.createObjectURL(new Blob(['const{ipcRenderer}=require("electron") ipcRenderer.on("ping",()=>{let currURL=window.location.href,prevURL="";setInterval(checkURL,250);function checkURL(){currURL=window.location.href;if(prevURL!==currURL){prevURL=window.location.href;main()}} async function main(){try{const videoID=/(?:\?v=)(.*?)(?:&|$)/i.exec(window.location.search)[1],response=await fetch(`https://www.youtube.com/get_video_info?video_id=${videoID}&el=detailpage`,{method:"GET"}),data=await response.text(),json=parseData(data);createLinks(json)}catch(e){ipcRenderer.sendToHost([])}} function parseData(data){const captured=/(?:player_response=)(.*?)(?:&|$)/i.exec(data)[1],json=JSON.parse(decodeURIComponent(captured));return json} async function createLinks(json){const details=json.videoDetails,formats=json.streamingData.formats,adaptive=json.streamingData.adaptiveFormats,adaptiveAudio=[];let row,target,i=0;for(;i<adaptive.length;i++){if(adaptive[i].mimeType.split(";")[0].split("/")[0]==="audio"){adaptiveAudio.push(adaptive[i])}} adaptiveAudio.sort((a,b)=>{return parseInt(b.contentLength)-parseInt(a.contentLength)});ipcRenderer.sendToHost(adaptiveAudio) formats.sort((a,b)=>{return parseInt(b.contentLength)-parseInt(a.contentLength)})}})'], {type: 'application/javascript'}))
     };
   },
   mounted() {
--- src/renderer/pages/Library/Songs.vue
+++ src/renderer/pages/Library/Songs.vue
@@ -2,24 +2,49 @@
   <center>
     <desk>
       <desk-row class="header">
-        <div class="divTableCell">Song Name</div>
-        <div class="divTableCell">Artist</div>
+        <div class="divTableCell"></div>
+        <div class="divTableCell">Artist(s)</div>
+        <div class="divTableCell">Title</div>
       </desk-row>
-      <desk-row>
-        <div class="divTableCell">&nbsp;</div>
-        <div class="divTableCell">&nbsp;</div>
-      </desk-row>
-      <desk-row>
-        <div class="divTableCell">&nbsp;</div>
-        <div class="divTableCell">&nbsp;</div>
-      </desk-row>
-      <desk-row>
-        <div class="divTableCell">&nbsp;</div>
-        <div class="divTableCell">&nbsp;</div>
+      <desk-row v-for="song of songList" v-bind:key="song">
+        <div class="divTableCell"><img v-bind:src="song.albumArt" width="50" height="50"></div>
+        <div class="divTableCell">
+          <a v-for="(artist, index) of song.artists" v-bind:key="artist">{{ artist }}<a v-if="song.artists.length > 1 && index != song.artists.length-1"> &amp; </a></a>
+        </div>
+        <div class="divTableCell">{{ song.title }}</div>
       </desk-row>
     </desk>
   </center>
 </template>
+
+<script>
+import Desk from "../../components/desk/Desk.vue"
+import DeskRow from "../../components/desk/Row.vue"
+export default {
+  components: {
+    Desk,
+    DeskRow
+  },
+  data() {
+    return {
+      songList: [
+        {
+          artists: ["Noisestorm"],
+          title: "Crab Rave",
+          featuring: [""],
+          albumArt: "https://assets.monstercat.com/releases/covers/Noisestorm%20-%20Crab%20Rave%20(Art).jpg"
+        },
+        {
+          artists: ["Madeon", "Porter Robinson"],
+          title: "Shelter",
+          featuring: ["A1 Pictures"],
+          albumArt: "http://i.imgur.com/06Obgip.jpg"
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="less">
 @import "./../../variables.less";
@@ -44,15 +69,4 @@
   font-weight: bold;
 }
 </style>
-
-<script>
-import Desk from "../../components/desk/Desk.vue"
-import DeskRow from "../../components/desk/Row.vue"
-export default {
-  components: {
-    Desk,
-    DeskRow
-  }
-}
-</script>
 
