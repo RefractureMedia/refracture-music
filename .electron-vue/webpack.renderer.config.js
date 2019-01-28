867d9f68fdc73a43bfb120fb74d8c5b0c683ba2d
MulverineX <mulverin3@gmail.com>
2019-01-27 23:11:52 -0700

    Julian did a dumb

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -35,68 +35,68 @@ let rendererConfig = {
   ],
   module: {
     rules: [{
-        test: /\.less$/,
-        use: ['vue-style-loader', 'css-loader', 'vue-style-loader!css-loader!less-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['vue-style-loader', 'css-loader']
-      },
-      {
-        test: /\.html$/,
-        use: 'vue-html-loader'
-      },
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.node$/,
-        use: 'node-loader'
-      },
-      {
-        test: /\.vue$/,
-        use: {
-          loader: 'vue-loader',
-          options: {
-            extractCSS: process.env.NODE_ENV === 'production',
-            loaders: {
-              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax=1',
-              scss: 'vue-style-loader!css-loader!sass-loader',
-              less: 'vue-style-loader!css-loader!less-loader'
-            }
-          }
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        use: {
-          loader: 'url-loader',
-          query: {
-            limit: 10000,
-            name: 'imgs/[name]--[folder].[ext]'
-          }
-        }
-      },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+      test: /\.less$/,
+      use: ['vue-style-loader', 'css-loader', 'less-loader']
+    },
+    {
+      test: /\.css$/,
+      use: ['vue-style-loader', 'css-loader']
+    },
+    {
+      test: /\.html$/,
+      use: 'vue-html-loader'
+    },
+    {
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    },
+    {
+      test: /\.node$/,
+      use: 'node-loader'
+    },
+    {
+      test: /\.vue$/,
+      use: {
+        loader: 'vue-loader',
         options: {
-          limit: 10000,
-          name: 'media/[name]--[folder].[ext]'
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        use: {
-          loader: 'url-loader',
-          query: {
-            limit: 10000,
-            name: 'fonts/[name]--[folder].[ext]'
+          extractCSS: process.env.NODE_ENV === 'production',
+          loaders: {
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax=1',
+            scss: 'vue-style-loader!css-loader!sass-loader',
+            less: 'vue-style-loader!css-loader!less-loader'
           }
         }
       }
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      use: {
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: 'imgs/[name]--[folder].[ext]'
+        }
+      }
+    },
+    {
+      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: 'media/[name]--[folder].[ext]'
+      }
+    },
+    {
+      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      use: {
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: 'fonts/[name]--[folder].[ext]'
+        }
+      }
+    }
     ]
   },
   node: {
