7422fd7146eed6847673db108b0d7c5f02b691ac
juliancruzsanchez <kickbutt.js@gmail.com>
2019-02-05 23:54:28 -0700

    Grr

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,10 @@
         <sidebar ref="sidebar" :state="state"></sidebar>
         <div class="wrap">
           <a v-if="state == 'closed'" class="sidebar_toggle" v-on:click="sidebar_toggle">≡</a>
-          <nav-bar/>
-          <library-search/>
+          <nav-bar />
+          <library-search />
           <div v-if="isDone">
-            <router-view
-              :library="library"
-              :currentSong="currentSong.meta"
-              :player="player"
-              :results="searchResults"
-            ></router-view>
+            <router-view :library="library" :currentSong="currentSong.meta" :player="player" :results="searchResults"></router-view>
           </div>
         </div>
       </div>
@@ -23,23 +18,23 @@
 </template>
 
 <script>
-import MediaBar from "./components/layout/MediaBar/MediaBar.vue";
-import Sidebar from "./components/layout/Sidebar.vue";
-import NavBar from "./components/layout/NavBar.vue";
-import MobileStyles from "./MobileStyles.css";
-import LibrarySearch from "./components/layout/LibrarySearch";
-import keys from "./keys.js";
-import router from "vue-router";
-import { getTimesFromMs, getTimestamp } from "./assets/js/timeManagement.js";
-import AppData from "./appData.js";
-import path from "path";
-import AdaptiveSourceFetcher from "./assets/js/asf.js";
-import request from "request";
-import { setTimeout } from "timers";
-import MobileDetect from "mobile-detect";
-import Search from "./Search.js";
-import ytsr from "ytsr";
-import htmlToJson from "html-to-json";
+import MediaBar from "./components/layout/MediaBar/MediaBar.vue"
+import Sidebar from "./components/layout/Sidebar.vue"
+import NavBar from "./components/layout/NavBar.vue"
+import MobileStyles from "./MobileStyles.css"
+import LibrarySearch from "./components/layout/LibrarySearch"
+import keys from "./keys.js"
+import router from "vue-router"
+import { getTimesFromMs, getTimestamp } from "./assets/js/timeManagement.js"
+import AppData from "./appData.js"
+import path from "path"
+import AdaptiveSourceFetcher from "./assets/js/asf.js"
+import request from "request"
+import { setTimeout } from "timers"
+import MobileDetect from "mobile-detect"
+import Search from "./Search.js"
+import ytsr from "ytsr"
+import htmlToJson from "html-to-json"
 
 export default {
   name: "refracture-music",
@@ -50,14 +45,14 @@ export default {
     LibrarySearch
   },
   data() {
-    return AppData;
+    return AppData
   },
   mounted() {
     const library = this.$data.library,
-      player = this.$data.player;
+      player = this.$data.player
     let songs = library.songs,
       albumsTemp = [],
-      artistsTemp = [];
+      artistsTemp = []
 
     for (let song of songs) {
       for (let artist of song.artists) {
@@ -68,145 +63,139 @@ export default {
             keys.lastfm +
             "&format=json",
           (err, res, dat) => {
-            let artist = JSON.parse(dat).artist;
-            if (err) console.error(err);
+            let artist = JSON.parse(dat).artist
+            if (err) console.error(err)
             else {
               if (!artistsTemp.includes(artist.name))
                 library.artists.push({
                   name: artist.name,
                   art: artist.image,
                   description: artist.summary
-                });
-              artistsTemp.push(artist.name);
+                })
+              artistsTemp.push(artist.name)
             }
           }
-        );
+        )
       }
       if (!albumsTemp.includes(song.album.title)) {
         library.albums.push({
           artists: song.album.artists,
           title: song.album.title,
           art: song.album.art
-        });
-        albumsTemp.push(song.album.title);
+        })
+        albumsTemp.push(song.album.title)
       } else
         for (let album of library.albums)
           if (album.name == song.album && !album.art.includes(song.art))
-            album.art.push(song.album.art);
-      this.$data.isDone = true;
+            album.art.push(song.album.art)
+      this.$data.isDone = true
     }
     player.ontimeupdate = () =>
-      (this.$data.currentSong.currentTime = getTimestamp(player.currentTime));
+      (this.$data.currentSong.currentTime = getTimestamp(player.currentTime))
     player.onerror = e => {
       throw Error(
         `Error: ${player.error.code}; details: ${player.error.message}`
-      );
-    };
+      )
+    }
     player.onchange = () => {
-      if (player.canPlayType == false)
-        throw Error("Cannot Play This File Type");
-    };
+      if (player.canPlayType == false) throw Error("Cannot Play This File Type")
+    }
     player.ondurationchange = () =>
-      (this.$data.currentSong.duration = getTimestamp(player.duration));
+      (this.$data.currentSong.duration = getTimestamp(player.duration))
 
-    setTimeout(() => this.setSong("LDU_Txk06tM"), 30);
+    setTimeout(() => this.setSong("LDU_Txk06tM"), 30)
 
     document
       .getElementsByClassName("songInput")[0]
       .addEventListener("keyup", e => {
         if (e.keyCode === 13) {
-          document.getElementById("submitSong").click();
-          e.preventDefault();
-          return false;
+          document.getElementById("submitSong").click()
+          e.preventDefault()
+          return false
         }
-      });
-    document.getElementsByClassName("songInput")[0].preventDefault();
+      })
+    document.getElementsByClassName("songInput")[0].preventDefault()
 
-    const detect = new MobileDetect(window.navigator.userAgent);
+    const detect = new MobileDetect(window.navigator.userAgent)
   },
   methods: {
     getCategory() {
-      return this.$router.path.split("/")[0];
+      return this.$router.path.split("/")[0]
     },
     sidebar_toggle() {
-      if (this.$data.state == "closed") this.$data.state = "open";
-      else this.$data.state = "closed";
+      if (this.$data.state == "closed") this.$data.state = "open"
+      else this.$data.state = "closed"
     },
     setSong(vidId) {
+      const player = this.$data.player
       AdaptiveSourceFetcher(vidId, res => {
-        this.$data.player.src = res[0].url;
-        this.$data.player.play();
-      });
+        player.src = res[0].url
+        player.play()
+      })
     },
     browseSearch() {
-      let input = document.getElementsByClassName("browseSearch")[0].value;
-      if (
-        !document.getElementsByClassName("browseSearch")[0].value.match(/:\/\//)
-      ) {
-        this.$data.searchResults = Search(
-          document.getElementsByClassName("browseSearch")[0].value,
-          true,
-          ""
-        );
-      } else {
-        this.setSong(parseYTURL(getSongInput()));
+      const search = document.getElementsByClassName("browseSearch")[0]
+      let input = document.getElementsByClassName("browseSearch")[0].value
+      if (!search.value.match(/:\/\//))
+        this.$data.searchResults = Search(!search.value, true, "")
+      else {
+        this.setSong(parseYTURL(getSongInput()))
       }
-      document.getElementsByClassName("browseSearch")[0].value = "";
+      search.value = ""
     },
     setCurrentSong(song) {
-      this.$data.currentSong = song;
-      this.updateSong(song);
+      this.$data.currentSong = song
+      this.updateSong(song)
     },
     updateSong() {
-      const song = this.$data.currentSong;
-      let song_string = song.artists + " " + song.title;
-      console.log(song_string);
-
-      ytsr(song_string, function(err, result) {
-        if (err) console.log(err);
+      const song = this.$data.currentSong,
+        player = this.$data.player
+      let song_string = song.artists + " " + song.title
+      ytsr(song_string, (err, result) => {
+        if (err) console.log(err)
         else {
-          console.log(result.items[0].link);
+          console.log(result.items[0].link)
           AdaptiveSourceFetcher(result.items[0].link.split("v=")[1], res => {
-            this.$data.player.src = res[0].url;
-            this.$data.player.play();
-          });
+            player.src = res[0].url
+            player.play()
+          })
         }
-      });
+      })
     },
     md() {
-      return new MobileDetect(window.navigator.userAgent);
+      return new MobileDetect(window.navigator.userAgent)
     }
   }
-};
+}
 window.notify = function(x) {
   const rid = Math.random()
     .toString(36)
-    .substring(7);
+    .substring(7)
 
   const t = document.createElement("div"),
-    s = document.createElement("span");
-  s.classList.add("close");
-  s.onclick = () => document.getElementById(`s-${rid}`).classList.remove("on");
-  s.innerHTML = "&times;";
-  t.classList.add("toast");
-  t.classList.add("on");
-  t.id = `s-${rid}`;
-  t.innerHTML = x;
-  t.appendChild(span);
+    s = document.createElement("span")
+  s.classList.add("close")
+  s.onclick = () => document.getElementById(`s-${rid}`).classList.remove("on")
+  s.innerHTML = "&times;"
+  t.classList.add("toast")
+  t.classList.add("on")
+  t.id = `s-${rid}`
+  t.innerHTML = x
+  t.appendChild(span)
 
-  document.body.appendChild(toast);
-};
+  document.body.appendChild(toast)
+}
 Array.prototype.shuffle = function() {
-  var input = this;
+  var input = this
   for (var i = input.length - 1; i >= 0; i--) {
-    var randomIndex = Math.floor(Math.random() * (i + 1));
-    var itemAtIndex = input[randomIndex];
+    var randomIndex = Math.floor(Math.random() * (i + 1))
+    var itemAtIndex = input[randomIndex]
 
-    input[randomIndex] = input[i];
-    input[i] = itemAtIndex;
+    input[randomIndex] = input[i]
+    input[i] = itemAtIndex
   }
-  return input;
-};
+  return input
+}
 
 function parseYTURL(input) {
   if (
@@ -214,20 +203,20 @@ function parseYTURL(input) {
     input.length == 11 &&
     (input.indexOf("youtube") == -1) | (input.indexOf("youtu.be") == -1)
   ) {
-    return input;
+    return input
   } else {
-    var regExp = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
-    var match = input.match(regExp);
+    var regExp = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
+    var match = input.match(regExp)
     if (match && match[5].length == 11) {
-      return match[5];
+      return match[5]
     } else {
-      notify("Could not extract video ID.");
+      notify("Could not extract video ID.")
     }
   }
 }
 function getSongInput() {
-  let url = document.getElementsByClassName("browseSearch")[0].value;
-  return url;
+  let url = document.getElementsByClassName("browseSearch")[0].value
+  return url
 }
 </script>
 
--- src/Search.js
+++ src/Search.js
@@ -22,7 +22,7 @@ export default function (input, outgoing, page) {
           request(
             "https://itunes.apple.com/lookup?id=" + track.collectionId,
             (err, res, dat) => {
-              if (err) console.log(err);
+              if (err) console.error(err);
               else {
                 let collectionArtist = JSON.parse(dat).results[0].artistName;
                 console.log(JSON.parse(dat).results);
@@ -36,8 +36,8 @@ export default function (input, outgoing, page) {
                     artists: collectionArtist.split(/ *[&X,] *| *x +| +x */),
                     title: track.collectionName,
                     art: [(
-                      track.artworkUrl100.replace("100x100bb.jpg", "1000x1000bb.jpg")
-                    ) /* Makes the artwork request be 1000px rather than 100*/
+                        track.artworkUrl100.replace("100x100bb.jpg", "1000x1000bb.jpg")
+                      ) /* Makes the artwork request be 1000px rather than 100*/
                       .toString()
                     ]
                   }
@@ -58,30 +58,32 @@ export default function (input, outgoing, page) {
       (err, res, dat) => {
         let raw_artists = JSON.parse(dat).results;
 
-        if (err) console.log(err)
-        else for (let artist of raw_artists) if (artistsTemp.includes(artist.name)) {
-          request(
-            "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" +
-            artist +
-            "&api_key=" +
-            keys.lastfm +
-            "&format=json",
-            (err, res, dat) => {
-              let artists = JSON.parse(dat).artist;
-              let parsed_artists = [];
-              for (let artist of artists) {
-                parsed_artists.push({
-                  name: artist.name,
-                  art: artist.image,
-                  description: artist.summary
-                });
-              }
-              console.log(parsed_artists);
-              if (err) console.error(err);
-              else output.artists = parsed_artists;
+        if (err) console.error(err)
+        else
+          for (let artist of raw_artists)
+            if (artistsTemp.includes(artist.name)) {
+              request(
+                "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" +
+                artist +
+                "&api_key=" +
+                keys.lastfm +
+                "&format=json",
+                (err, res, dat) => {
+                  let artists = JSON.parse(dat).artist;
+                  let parsed_artists = [];
+                  for (let artist of artists) {
+                    parsed_artists.push({
+                      name: artist.name,
+                      art: artist.image,
+                      description: artist.summary
+                    });
+                  }
+                  console.log(parsed_artists);
+                  if (err) console.error(err);
+                  else output.artists = parsed_artists;
+                }
+              );
             }
-          );
-        }
       }
     );
 
@@ -95,8 +97,8 @@ export default function (input, outgoing, page) {
             artists: album.artistName.split(/ *[&X,] *| *x +| +x */),
             title: album.collectionName,
             art: [(
-              album.artworkUrl100.replace("100x100bb.jpg", "1000x1000bb.jpg")
-            ) /* Makes the artwork request be 1000px rather than 100*/
+                album.artworkUrl100.replace("100x100bb.jpg", "1000x1000bb.jpg")
+              ) /* Makes the artwork request be 1000px rather than 100*/
               .toString()
             ]
           });
@@ -108,26 +110,27 @@ export default function (input, outgoing, page) {
     );
     return output;
     // do youtube result requesting & filter out `songsTemp.includes(parsed_result.title)` (same thing for soundcloud when we get around to doing soundcloud)
-  } /*else {
-    if (page == "Songs")
-      for (let song of this.$data.library.songs) {
-        if (song.title.includes(input))
-          output.songs.push(song);
-      }
-    if (page == "Artists")
-      for (let artist of this.$data.library.artists) {
-        if (artist.name.includes(input))
-          output.artists.push(artist);
-      }
-    if (page == "Albums")
-      for (let album of this.$data.library.albums) {
-        if (album.title.includes(input))
-          output.albums.push(album);
-      }
-    if (page == "Playlists")
-      for (let playlist of this.$data.library.playlist) {
-        if (playlist.name.includes(input))
-          output.playlists.push(playlist);
-      }
-  }*/
+  }
+  /*else {
+     if (page == "Songs")
+       for (let song of this.$data.library.songs) {
+         if (song.title.includes(input))
+           output.songs.push(song);
+       }
+     if (page == "Artists")
+       for (let artist of this.$data.library.artists) {
+         if (artist.name.includes(input))
+           output.artists.push(artist);
+       }
+     if (page == "Albums")
+       for (let album of this.$data.library.albums) {
+         if (album.title.includes(input))
+           output.albums.push(album);
+       }
+     if (page == "Playlists")
+       for (let playlist of this.$data.library.playlist) {
+         if (playlist.name.includes(input))
+           output.playlists.push(playlist);
+       }
+   }*/
 }
