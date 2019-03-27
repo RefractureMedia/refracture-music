1a831846f62cfe5b6a4edf2ee15f2fdb5bfd189e
MulverineX <mulverin3@gmail.com>
2019-03-26 22:29:24 -0600

    lambda for netlify

--- src/assets/js/yt_source.js
+++ src/assets/js/yt_source.js
@@ -48,6 +48,29 @@ export default (id, callback) => {
     }
 }
 
+exports.handler = (event, context, callback) => {
+    request(
+        `http://www.youtube.com/get_video_info?html5=1&video_id=${event.queryStringParameters.id}&el=detailpage`,
+        (err, res, dat) => {
+            if (err) {
+                console.log(err);
+                callback(null, { statusCode: 404, body: "Failed"})
+            }
+            else {
+                const bodyParams = new URLSearchParams(dat);
+                let links = getLinks(JSON.parse(bodyParams.get("player_response")).streamingData.adaptiveFormats);
+                callback(null, { statusCode: 200, body: JSON.stringify({
+                    title: bodyParams.get("title"),
+                    channel: bodyParams.get("author"),
+                    thumb: bodyParams.get("thumbnail_url"),
+                    links: links
+                }) });
+                
+            }
+        }
+    )
+}
+
 function getLinks(adapt_formats) {
     let srcs = [],
         i = 0;
