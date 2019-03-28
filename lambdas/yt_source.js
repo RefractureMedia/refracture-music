b64da035f6469d31fdd9acefa58204ed5c37e2a6
Mulvey <dlmf1ccd@thompsonschools.org>
2019-03-28 15:15:15 -0600

    Maybe now?

--- lambdas/yt_source.js
+++ lambdas/yt_source.js
@@ -1,4 +1,4 @@
-const http = require("http");
+const request = require("request");
 const {
   URLSearchParams
 } = require('url');
@@ -6,40 +6,28 @@ global.URLSearchParams = URLSearchParams;
 
 exports.handler = async (event, context) => {
   return new Promise((resolve, reject) => {
-    http.get(
-      `http://www.youtube.com/get_video_info?html5=1&video_id=${event.queryStringParameters.id}&el=detailpage`, {},
-      (res) => {
-        res.setEncoding('utf8');
-        let data = '';
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-        let foo = false;
-        if (foo) {
-          reject("foo bar");
+    request(
+      `http://www.youtube.com/get_video_info?html5=1&video_id=${event.queryStringParameters.id}&el=detailpage`,
+      (err, res, dat) => {
+        if (!err) {
+          const bodyParams = new URLSearchParams(dat);
+          let links = getLinks(JSON.parse(bodyParams.get("player_response")).streamingData.adaptiveFormats);
+          resolve({
+            statusCode: 200,
+            body: JSON.stringify({
+              title: bodyParams.get("title"),
+              channel: bodyParams.get("author"),
+              thumb: bodyParams.get("thumbnail_url"),
+              links: links
+            })
+          });
+        } else {
+          reject(err.message);
         }
-        res.on('end', () => {
-          if (res.complete) {
-            const bodyParams = new URLSearchParams(data);
-            let links = getLinks(JSON.parse(bodyParams.get("player_response")).streamingData.adaptiveFormats);
-            resolve(
-              {
-                statusCode: 200,
-                body: JSON.stringify({
-                  title: bodyParams.get("title"),
-                  channel: bodyParams.get("author"),
-                  thumb: bodyParams.get("thumbnail_url"),
-                  links: links
-                })
-              }
-            );
-          }
-        });
-      },
-      (err) => {
-        console.log(err);
       }
-    );
+    ).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
   })
 }
 
