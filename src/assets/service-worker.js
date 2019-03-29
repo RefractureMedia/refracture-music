e59631663ddd76f3850daf1375b50a4fd487797b
Mulvey <dlmf1ccd@thompsonschools.org>
2019-03-29 08:55:41 -0600

    Fetch Handler

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -1,1 +1,13 @@
 console.log("Service Working");
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+    .then(cache => cache.match(event.request, {
+      ignoreSearch: true
+    }))
+    .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
