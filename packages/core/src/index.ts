abcec101abfcd04e64acf1b643696cd7fb02b5a0
Mulverine <mulverin3@gmail.com>
2022-05-31 01:08:25 -0600

    🚧 Built out ItemManager, OOP & Generics are fun

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,7 +4,13 @@ class MusicCore {
 
     plugins: PluginManager;
 
-    tracks: TrackManager;
+    tracks: ItemManager<'track'>;
+
+    artists: ItemManager<'artist'>;
+
+    albums: ItemManager<'albums'>;
+
+    playlists: ItemManager<'playlist'>;
 
     constructor() {
 
@@ -32,16 +38,24 @@ export class AccountClass {
 
     plugin_data: PluginData;
 
+    // TypeScript doesn't enforce those results... w h y
+    plugins_resolved: Promise<PromiseSettledResult<'resolved' | 'rejected' | 'this_sucks'>[]>
+
     constructor (name: string, avatar: string, imports?: PluginData) {
         this.name = name;
 
         this.avatar = avatar;
 
         this.id = uuid();
+        
+        const resolving_plugins = [];
 
-        if (imports) for (const plugin of Music.plugins.plugin_map.entries()) {
-            plugin[1].resolveImport(this.plugin_data[plugin[0]], imports[plugin[0]]);
+        if (imports) for (const plugin of Music.plugins.plugin_map.entries()) if (plugin[1].resolveImport) {
+            resolving_plugins.push(plugin[1].resolveImport(this.plugin_data[plugin[0]], imports[plugin[0]]));
         }
+
+        // Lets us provide an event.
+        this.plugins_resolved = Promise.allSettled(resolving_plugins);
     }
 }
 
@@ -67,83 +81,132 @@ export class PluginClass {
     /**
      * Resolves plugin account data imports (ie. during account creation or in account settings)
      */
-    resolveImport: (existing: RawData, importing: RawData) => {};
+    resolveImport: (existing: RawData, importing: RawData) => ({} | Promise<any>);
 }
 
-function getResolveTrack (track_id: TrackIdentifier) {
+function getItem<ItemType, ItemIdentifierType> (item_type: ItemType, item_id: ItemIdentifierType) {
     return (async () => {
-        this.track_map[track_id] = new TrackClass(JSON.parse(await DB.request('track', track_id)));
-        return this.track_map[track_id];
-    }) as ResolveTrack<TrackIdentifier>;
+        this.item_map[item_id] = new ItemClass(item_type, item_id, JSON.parse(await DB.request(item_type, item_id)));
+        return this.item_map[item_id] as ItemClass<ItemType, ItemIdentifierType>;
+    });
 }
 
-class TrackManager {
-    track_map: TrackMap;
+type ItemTypes = 'track' | 'artist' | 'album' | 'playlist';
 
-    accessed_tracks: Map<TrackIdentifier, AccessIdentifier[]>;
+class ItemManager<ItemType = ItemTypes> {
+    type: ItemTypes;
 
-    async access (track_id: TrackIdentifier, access_id: AccessIdentifier) {
-        const track = this.track_map[track_id];
+    item_map: ItemMap<ItemType>;
 
-        if (this.accessed_tracks[track_id]) this.accessed_tracks[track_id].push(access_id);
-        else this.accessed_tracks[track_id] = [ access_id ];
+    accessed_items: Map<ItemIdentifierType, AccessIdentifierType[]>;
 
-        if (track instanceof TrackClass) return track;
-        else {
-            const resolved_track = await track();
-            this.track_map[track_id] = resolved_track;
-            return resolved_track;
+    async access (item_id: ItemIdentifierType, access_id?: AccessIdentifierType) {
+        if (!access_id) access_id = uuid();
+
+        if (this.item_map[item_id]) {
+            this.accessed_items[item_id].push(access_id);
+
+            return this.item_map[item_id];
+        } else {
+            this.accessed_items[item_id] = [ access_id ];
+
+            const resolved_item = new ItemClass(this.type, item_id, JSON.parse(await DB.request(this.type, item_id)));
+
+            this.item_map[item_id] = resolved_item;
+
+            return this.item_map[item_id];
         }
     }
 
-    free (access_id: AccessIdentifier, track_id?: TrackIdentifier) {
-        if (track_id) {
+    async accessFromAll (sort_order: string /* TODO: Wrapper class for complex sorting */, count: number, start_at: number, access_id?: AccessIdentifierType) {
+        if (!access_id) access_id = uuid();
+        
+        const results: ItemClass<ItemType, unknown>[] = [];
+
+        JSON.parse(await DB.request(`${this.type}_sorted`, count, start_at, sort_order)).forEach((id: string, data: RawData) => {
+            if (this.item_map[id]) {
+                this.accessed_items[id].push(access_id)
+                this.item_map[id] = new ItemClass(this.type, id, data);
+
+                results.push(this.item_map[id]);
+            } else {
+                this.accessed_items[id] = [ access_id ];
+                results.push(this.item_map[id]);
+            }
+        });
+
+        // TypeScript why
+        return [results, access_id] as [ItemClass<ItemType, unknown>[], AccessIdentifierType];
+    }
+
+
+    // In progress TODO
+    async accessSet (item_ids: ItemIdentifierType[], access_id?: AccessIdentifierType) {
+        if (!access_id) access_id = uuid();
+
+        const results: ItemClass<ItemType, unknown>[] = [];
+
+        JSON.parse(await DB.request('item_set', JSON.stringify(
+            item_ids.filter(id => ![...this.item_map.keys()].includes(id))
+        ))).map(item => cache[item[0]] = new ItemClass(item[1]));
+
+        if (cache_count > item_list.length) for (const [i, item_id] of item_list.entries()) {
+            if (i < cache_count) this.item_map[item_id] = cache[item_id];
+            else this.item_map[item_id] = getResolveItem(item_id);
+        }
+    }
+
+    /**
+     * 
+     * @param access_id The database access id of the current scope received from `ItemManager.access[0]`.
+     * @param item_id Optional. If a single item has been accessed provide this to optimize logic.
+     */
+    free (access_id: AccessIdentifierType, item_id?: ItemIdentifierType) {
+        if (item_id) {
             // uhhhhh, duh!? wtf typescript
-            this.accessed_tracks[track_id] = (this.accessed_tracks[track_id] as AccessIdentifier[]).filter((id) => id !== track_id);
+            this.accessed_items[item_id] = (this.accessed_items[item_id] as AccessIdentifierType[]).filter((id) => id !== item_id);
             
-            if (!this.accessed_tracks[track_id].length) {
-                delete this.accessed_tracks[track_id];
-                this.track_map[track_id] = getResolveTrack(track_id);
+            if (!this.accessed_items[item_id].length) {
+                delete this.accessed_items[item_id];
+                this.item_map[item_id] = getItem(this.type, item_id);
             }
         }
-        else for (const [track, access] of this.accessed_tracks.entries()) {
-            if (access.includes(track_id)) {
-                this.accessed_tracks[track] = (this.accessed_tracks[track] as AccessIdentifier[]).filter((id) => id !== track);
+        else for (const [item, access] of this.accessed_items.entries()) {
+            const access_index = access.indexOf(access_id);
+
+            if (access_index !== -1) {
+                this.accessed_items[item].splice(access_index, 1);
         
-                if (!this.accessed_tracks[track].length) {
-                    delete this.accessed_tracks[track];
-                    this.track_map[track] = getResolveTrack(track);
+                if (!this.accessed_items[item].length) {
+                    delete this.accessed_items[item];
+                    this.item_map[item] = getItem(this.type, item);
                 }
             }
         }
     }
 
-    async build(track_list: TrackIdentifier[], sort_order, cache_count: number) {
-        this.track_map = new Map() as TrackMap;
+    constructor (type: ItemTypes) {
+        this.type = type;
 
-        const cache = new Map<TrackIdentifier, TrackClass<TrackIdentifier>>();
+        this.item_map = new Map();
 
-        JSON.parse(await DB.request('tracks', sort_order, cache_count)).map(track => cache[track[0]] = new TrackClass(track[1]));
-
-        if (cache_count > track_list.length) for (const [i, track_id] of track_list.entries()) {
-            if (i < cache_count) this.track_map[track_id] = cache[track_id];
-            else this.track_map[track_id] = getResolveTrack(track_id);
-        }
+        this.accessed_items = new Map();
     }
 }
 
-type AccessIdentifier = string;
+type AccessIdentifierType = string;
 
-type TrackMap = Map<TrackIdentifier, TrackClass<TrackIdentifier> | ResolveTrack<TrackIdentifier>>;
+type ItemMap<T> = Map<ItemIdentifierType, ItemClass<T, ItemIdentifierType>>;
 
-type TrackIdentifier = string;
+type ItemIdentifierType = string;
 
-type ResolveTrack<T> = () => Promise<TrackClass<T>>;
+// TODO: Fix TypeScript being garbage
+class ItemClass<ItemType = ItemTypes, ItemIdentifier = ItemIdentifierType> {
+    type: ItemType;
 
-class TrackClass<TrackIdentifier> {
-    id: TrackIdentifier
+    id: ItemIdentifierType;
 
-    constructor (data: RawData) {
+    constructor (type: ItemType, id: ItemIdentifier, data: RawData) {
 
     }
 }
